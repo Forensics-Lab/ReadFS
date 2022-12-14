0c71301fcf03c006bd58684d8291a3753758e8d8
1Doomdie1 <horia.todoran@yahoo.com>
2022-12-14 17:15:45 +0000

    Ability to read boot secor of ReFS forensic images

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from json5 import load
 from typing import Union
-from struct import pack, unpack
+from struct import unpack
 
 
 def getBytes(byteRange: Union[list[bytes, int], tuple[bytes, int], set[bytes, int]], offset: Union[int, bytes] = 0) -> bytes:
@@ -10,31 +10,30 @@ def getBytes(byteRange: Union[list[bytes, int], tuple[bytes, int], set[bytes, in
         data = file.read(abs(byteRange[0] - byteRange[1]))
     return data
 
+
 def getFile(filePath:str, mode:str = "r") -> dict:
     with open(filePath, mode) as file:
         data = load(file)
     return data
 
 
-def toDecimal(_bytes: bytes, order: str = "<") -> int:
-    return unpack(f"{order}I", _bytes)[0]
+def toDecimal(_bytes: bytes, order: str = "<", size="I") -> int:
+    return unpack(f"{order}{size}", _bytes)[0]
 
-
-def superBlock(SectorsPerCluster:bytes, BytesPerSector:bytes) -> int:
-    return 0x1e * toDecimal(SectorsPerCluster) * toDecimal(BytesPerSector)
 
 def main():
-    REFS_Offsets = getFile("D:\\Uni\\Third Year\\CST3590 Individual Project\\fsread\\REFS_offsets.json5")
+    REFS_Offsets = getFile("REFS_offsets.json5")
     REFS_Boot_Sector = REFS_Offsets["bootSector"]
-    REFS_Page_Header = REFS_Offsets["pageHeader"]
+    print("<<=====================Boot Sector=====================>>\n"
+          f"[+] Volume Boot Sector Size: {toDecimal(getBytes(REFS_Boot_Sector['sizeOfVBR']), size='h')} bytes\n"
+          f"[+] Sectors per Cluster: {toDecimal(getBytes(REFS_Boot_Sector['sectorsPerCluster']))}\n"
+          f"[+] Bytes per Sector: {toDecimal(getBytes(REFS_Boot_Sector['bytesPerSector']))}\n"
+          f"[+] Number of Sectors: {toDecimal(getBytes(REFS_Boot_Sector['sectorCount']), size='q'):,}\n"
+          f"[+] Volume size: {toDecimal(getBytes(REFS_Boot_Sector['sectorCount']), size='q') * toDecimal(getBytes(REFS_Boot_Sector['bytesPerSector'])):,} bytes\n"
+          f"[+] Page size: {toDecimal(getBytes(REFS_Boot_Sector['bytesPerContainer']), size='q'):,} bytes\n"
+          f"[+] Volume Serial Number: {getBytes(REFS_Boot_Sector['volumeSerialNumber'])[::-1].hex().upper()}\n"
+          "<<=====================================================>>\n")
 
-
-    spc = getBytes(REFS_Boot_Sector["sectorsPerCluster"])
-    bps = getBytes(REFS_Boot_Sector["bytesPerSector"])
-    
-    superBlockOffset = superBlock(spc, bps)
-    pageSig = getBytes(REFS_Page_Header["pageSignature"], superBlockOffset)
-    print(pageSig)
 
 if __name__ == '__main__':
     main()
