ed239202dc0a7e039b2f9aa96f30cf397dc74459
1Doomdie1 <horia.todoran@yahoo.com>
2022-12-15 09:19:28 +0000

    adding more inormation to bootsector output

--- ReFS/bootsector.py
+++ ReFS/bootsector.py
@@ -7,51 +7,54 @@ class BootSector():
         self.byteArray = byteArray
         self.formater = Formater()
     
-    def assemblyCode(self):
+    def assemblyCode(self) -> str:
         return self.formater.toHex(self.byteArray[0x0:0x03])
     
-    def REFSSifniture(self):
+    def REFSSifniture(self) -> str:
         return self.formater.toString(self.byteArray[0x03:0x0B])
     
-    def mustBeZero(self):
+    def mustBeZero(self) -> int:
         return self.formater.toDecimal(self.byteArray[0x0B:0x10])
 
-    def FSRSIdentifier(self):
+    def FSRSIdentifier(self) -> str:
         return self.formater.toString(self.byteArray[0x10:0x14])
 
-    def sizeOfVBR(self):
+    def sizeOfVBR(self) -> int:
         return self.formater.toDecimal(self.byteArray[0x14:0x16])
     
-    def checksum(self):
+    def checksum(self) -> str:
         return self.formater.toHex(self.byteArray[0x16:0x18])
     
-    def sectorCount(self):
+    def sectorCount(self) -> int:
         return self.formater.toDecimal(self.byteArray[0x18:0x20])
     
-    def bytesPerSector(self):
+    def bytesPerSector(self) -> int:
         return self.formater.toDecimal(self.byteArray[0x20:0x24])
     
-    def sectorsPerCluster(self):
+    def sectorsPerCluster(self) -> int:
         return self.formater.toDecimal(self.byteArray[0x24:0x28])
     
-    def ReFSVersion(self):
+    def ReFSVersion(self) -> str:
         major, minor = self.byteArray[0x28:0x2A]
         return f"{major}.{minor}"
     
-    def volumeSerialNumber(self):
+    def volumeSerialNumber(self) -> str:
         return self.formater.toHex(self.byteArray[0x38:0x40])
     
-    def bytesPerContainer(self):
+    def bytesPerContainer(self) -> int:
         return self.formater.toDecimal(self.byteArray[0x40:0x48])
 
     def info(self) -> str:
         return "<<=====================Boot Sector=====================>>\n"\
               f"[+] ReFS Version: {self.ReFSVersion()}\n"\
+              f"[+] Super Block Offset: {0x1e * self.sectorsPerCluster() * self.bytesPerSector()} bytes\n"\
               f"[+] VBR Size: {self.sizeOfVBR()} bytes\n"\
-              f"[+] Sectors per Cluster: {self.sectorsPerCluster()}\n"\
               f"[+] Bytes per Sector: {self.bytesPerSector()}\n"\
+              f"[+] Page size: {self.sectorsPerCluster() * self.bytesPerSector():,} bytes\n"\
+              f"[+] Sectors per Cluster: {self.sectorsPerCluster()}\n"\
+              f"[+] Number of Clusters: {self.sectorCount()//self.sectorsPerCluster():,}\n"\
               f"[+] Number of Sectors: {self.sectorCount():,}\n"\
               f"[+] Volume size: {self.bytesPerSector() * self.sectorCount():,} bytes\n"\
-              f"[+] Page size: {self.bytesPerContainer():,} bytes\n"\
+              f"[+] Container size: {self.bytesPerContainer():,} bytes\n"\
               f"[+] Volume Serial Number: {self.volumeSerialNumber()}\n"\
               "<<=====================================================>>\n"
