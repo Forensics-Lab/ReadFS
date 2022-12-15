1e7706657a9abb0cdaa6bbda6f95f3b060d7be25
1Doomdie1 <horia.todoran@yahoo.com>
2022-12-15 21:56:14 +0000

    Added some "helpful" coments :))

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ReFS.superblock import Superblock
 from ReFS.checkpoint import Checkpoint
 
 def getBytes(byteRange: Union[list[bytes, int], tuple[bytes, int], set[bytes, int]], offset: Union[int, bytes] = 0) -> bytes:
-    path = "samples/logical_refs2.001"
+    path = "samples/logical_refs_4Kb.001"
     with open(path, "rb") as file:
         file.seek(offset+ byteRange[0])
         data = file.read(abs(byteRange[0] - byteRange[1]))
@@ -17,9 +17,15 @@ def main():
     sb = Superblock(getBytes([0x0, pageSize], sbo))
     chkoff = sb.checkpointOffset()[0]
     checkpoint = Checkpoint(getBytes([0x0, pageSize], chkoff))
-    print(bootSector.info())
-    print(sb.info())
-    print(checkpoint.info())
+
+    # Uncomment line 21 to get information about ReFS's bootsector based on your logical forensic image 
+    # print(bootSector.info())
+    
+    # Uncomment line 24 to get information about ReFS's superblock based on your logical forensic image
+    # print(sb.info())
+    
+    # Uncomment line 28 to get information about ReFS's checkpoint based on your logical forensic image
+    # print(checkpoint.info())
 
 if __name__ == '__main__':
     main()
