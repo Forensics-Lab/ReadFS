a84bf40e0ae02cc61739213f3650e09ef640e985
1Doomdie1 <horia.todoran@yahoo.com>
2023-03-08 15:52:58 +0000

    better arguments description and usage update;

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ from ReFS.superblock import Superblock
 from ReFS.checkpoint import Checkpoint
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help = "Path to file.", metavar='', required=True)
-parser.add_argument("-ii", "--image_info", help = "Displays information about the partition found in the Boot Sector.", action="store_true")
-parser.add_argument("-bi", "--block_info", help = "Retries separate bytes.", metavar='', choices=["superblock", "checkpoint"])
-parser.add_argument("--node", help = "Specifie the offset to a node", metavar='', type=int)
-parser.add_argument("--info", help = "Prints out data about the node (--node flag must be set)", action="store_true")
-parser.add_argument("--entry", help = "Prints out data of a single entrie (--node flag must be set)", metavar='', type=int)
-parser.add_argument("--entries", help = "Prints out data about all entries from a Node (--node flag must be set)", action="store_true")
+parser.add_argument("-f", "--file", help="Path to file", metavar='', required=True)
+
+infoArgs = parser.add_argument_group("Info flags").add_mutually_exclusive_group()
+infoArgs.add_argument("-ii", "--image_info", help="Displays information about the partition found in the Bootsector", action="store_true")
+infoArgs.add_argument("-bi", "--block_info", help="Carves out data about either Superblock or Checkpoint", metavar='', choices=["superblock", "checkpoint"])
+infoArgs.add_argument("-n", "--node", help="Specify the offset to a node", metavar='', type=int)
+
+nodeArgs = parser.add_argument_group("Node required flags").add_mutually_exclusive_group()
+nodeArgs.add_argument('--info', help='Prints out data about the node', action='store_true')
+nodeArgs.add_argument('--entry', help='Prints out data of a single entry', metavar='', type=int)
+nodeArgs.add_argument('--entries', help='Prints out data about all entries from a Node', action='store_true')
+
+infoArgs.required = True if not parser.parse_args().node else False
+nodeArgs.required = True if parser.parse_args().node else False
+
 args = parser.parse_args()
 
 def main():
@@ -29,6 +37,7 @@ def main():
             print(superblock.info())
         elif args.block_info == "checkpoint":
             print(checkpoint.info())
+        
     if args.node:
         node = Node(args.file, [0x0, clusterSize], args.node)
         if args.info:
@@ -39,5 +48,5 @@ def main():
         if args.entry:
             print(node.indexEntries().getEntries()[args.entry])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
