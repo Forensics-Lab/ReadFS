84dd6f43fbb279f8811d54caf654c63f5f34af9e
1Doomdie1 <horia.todoran@yahoo.com>
2023-03-27 23:54:47 +0100

    removed unecessary class attribute

--- Managers/EvidenceFile/reader.py
+++ Managers/EvidenceFile/reader.py
@@ -3,7 +3,6 @@ from ..Bytes import Formater
 class Reader():
     def __init__(self, file: str) -> None:
         self.file = file
-        self.formater = Formater()
 
     def getBytes(self, byteRange: int = 65536, offset: int= 0) -> bytes:
         with open(self.file, "rb") as file:
