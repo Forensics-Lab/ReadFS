6131f4518b5271d17bcf0a42f363791357d3a42d
1Doomdie1 <horia.todoran@yahoo.com>
2023-08-05 14:48:19 +0100

    Implemented the Import case feature; Added export archive signing;

--- GUI/Managers/case.py
+++ GUI/Managers/case.py
@@ -4,13 +4,27 @@ from datetime import datetime
 from json     import dumps, loads
 from os       import path, mkdir, listdir, rename, getcwd, remove
 
+
 class Case_Manager:
-    def __init__(self, ):
+    def __init__(self):
         self._case_status = None
         self.date         = int(round(datetime.now().timestamp()))
 
     def _metadata_structure(self):
         return {"case_id": None, "author": None, "date_created": None, "date_modified": None}
+
+    def _sign_export_zip(self, path_to_zip):
+        # The key in the future will change
+        # This is just a POC
+        with open(path_to_zip, 'ab') as zip_file:
+            zip_file.write(b"abcdefghij")
+
+    def _check_zip_sig(self, path_to_zip):
+        # The key in the future will change
+        # This is just a POC
+        with open(path_to_zip, 'rb') as zip_file:
+            zip_file.seek(-10, 2)
+            return zip_file.read() == b"abcdefghij"
 
     def root_folder(self):
         return path.abspath("data/cases/")
@@ -64,17 +78,17 @@ class Case_Manager:
     def status(self):
         return self._case_status
 
+    def reset_status(self):
+        self._case_status = None
+
     def delete(self, case_id):
         rmtree(path.join(self.root_folder(), case_id))
         self._case_status = "SUCCESS"
 
     def list(self):
-        return listdir(self.root_folder())
+        return [dir_ for dir_ in listdir(self.root_folder()) if not "." in dir_]
 
     def open(self, name):
-        ...
-
-    def close(self, name):
         ...
 
     def export(self, dir_to_export, export_path):
@@ -84,13 +98,34 @@ class Case_Manager:
                 shutil.make_archive(export_name, "zip", dir_to_export)
                 src = path.join(getcwd(), f"{export_name}.zip")
                 dst = path.join(export_path, f"{export_name}.zip")
+
+                # Sign the zip file
+                self._sign_export_zip(src)
+
+                # Move the zip file to the export path
                 rename(src, dst)
+
                 self._case_status = "SUCCESS"
             except FileExistsError:
                 remove(path.join(getcwd(), f"{export_name}.zip"))
                 self._case_status = "EXPORT_PATH_ALREADY_EXISTS"
         else:
-            self._case_status = "EXPORT_PATH_NOT_SPECIFIED"
+            self._case_status = "EXPORT_PATH_WINDOW_CLOSED"
 
-    def import_(self, path):
-        ...
+    def import_(self, path_to_zip):
+        # Check if path is not empty
+        if path_to_zip:
+            try:
+                # Check if zip file is a ReadFS export case
+                is_valid = self._check_zip_sig(path_to_zip)
+                if is_valid:
+                    src = path_to_zip
+                    dst = path.join(self.root_folder(), path.basename(path_to_zip))
+                    shutil.unpack_archive(src, dst[:-11], "zip")
+                    self._case_status = "SUCCESS"
+                elif not is_valid:
+                    self._case_status = "BAD_IMPORT_FILE"
+            except FileNotFoundError:
+                self._case_status = "IMPORT_FILE_NOT_FOUND"
+        elif not path_to_zip:
+            self._case_status = "EMPTY_IMPORT_PATH"
